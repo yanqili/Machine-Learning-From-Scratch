d29aa69840d04f066a4e96f71cad00de3e967ee8
LYQ <949238389@qq.com>
2020-04-19 17:53:50 +0800

    complete Decison tree model

---
+++ utils/data_manipulation.py
@@ -0,0 +1,14 @@
+import numpy as np
+def divide_on_feature(X, feature_i, threshold):
+    """ Divide dataset based on if sample value on feature index is larger than
+        the given threshold """
+    split_func = None
+    if isinstance(threshold, int) or isinstance(threshold, float):
+        split_func = lambda sample: sample[feature_i] >= threshold
+    else:
+        split_func = lambda sample: sample[feature_i] == threshold
+
+    X_1 = np.array([sample for sample in X if split_func(sample)])
+    X_2 = np.array([sample for sample in X if not split_func(sample)])
+
+    return np.array([X_1, X_2])
---
+++ utils/data_operation.py
@@ -0,0 +1,23 @@
+from __future__ import division
+import numpy as np
+import math
+import sys
+
+def calculate_entropy(y):
+    """ Calculate the entropy of label array y """
+    log2 = lambda x: math.log(x) / math.log(2)
+    unique_labels = np.unique(y)
+    entropy = 0
+    for label in unique_labels:
+        count = len(y[y == label])
+        p = count / len(y)
+        entropy += -p * log2(p)
+    return entropy
+
+def calculate_variance(X):
+    """ Return the variance of the features in dataset X """
+    mean = np.ones(np.shape(X)) * X.mean(0)
+    n_samples = np.shape(X)[0]
+    variance = (1 / n_samples) * np.diag((X - mean).T.dot(X - mean))
+    
+    return variance
